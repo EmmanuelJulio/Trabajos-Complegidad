a8108311f1de7f921ad75a7ac3091292239f045e
EmmaLap <EmmaLap@EMMALAP>
2019-09-23 22:30:30 -0300

    todo bien 3.0

---
+++ Trabajo final Comp/ArbolGeneral.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trabajo_final_Comp
+{
+    public class ArbolGeneral<T>
+    {
+
+        private NodoGeneral<T> raiz;
+
+        public ArbolGeneral(T dato)
+        {
+            this.raiz = new NodoGeneral<T>(dato);
+        }
+
+        public ArbolGeneral(NodoGeneral<T> nodo)
+        {
+            this.raiz = nodo;
+        }
+
+        private NodoGeneral<T> getRaiz()
+        {
+            return raiz;
+        }
+
+        public T getDatoRaiz()
+        {
+            return this.getRaiz().getDato();
+        }
+
+        public List<ArbolGeneral<T>> getHijos()
+        {
+            List<ArbolGeneral<T>> temp = new List<ArbolGeneral<T>>();
+            foreach (var element in this.raiz.getHijos())
+            {
+                temp.Add(new ArbolGeneral<T>(element));
+            }
+            return temp;
+        }
+
+        public void agregarHijo(ArbolGeneral<T> hijo)
+        {
+            this.raiz.getHijos().Add(hijo.getRaiz());
+        }
+
+        public void eliminarHijo(ArbolGeneral<T> hijo)
+        {
+            this.raiz.getHijos().Remove(hijo.getRaiz());
+        }
+
+        public bool esVacio()
+        {
+            return this.raiz == null;
+        }
+
+        public bool esHoja()
+        {
+            return this.raiz != null && this.getHijos().Count == 0;
+        }
+
+        public int altura()
+        {
+            int altura = 0;
+            if (esHoja())
+                return 0;
+            foreach(ArbolGeneral<T> hy in this.getHijos())
+            {
+                altura = Math.Max(altura, hy.altura());
+                
+            }
+
+            return altura+1;
+        }
+
+
+        public int nivel(T dato)
+        {
+
+            if (this.getDatoRaiz().Equals(dato))
+            {
+                return 0;
+            }
+            else
+            {
+                foreach (ArbolGeneral<T> h in getHijos())
+                    
+                {
+                   
+                }
+            }
+            return 0;
+        }
+
+         public int ancho()
+            {
+                int ancho = 0;
+                return ancho;
+            }
+          
+        //public bool include (T dato)
+        //{
+        //    if (this.getDatoRaiz().Equals(dato))
+        //        return true;
+        //    else {
+        //        if (this.)
+        //         }
+            
+        //}
+    }
+}
+//renzo.angeloro@gmail.com
---
+++ Trabajo final Comp/NodoGeneral.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trabajo_final_Comp
+{
+    public class NodoGeneral<T>
+    {
+        private T dato;
+        private List<NodoGeneral<T>> hijos;
+
+        public NodoGeneral(T dato)
+        {
+            this.dato = dato;
+            this.hijos = new List<NodoGeneral<T>>();
+        }
+
+        public T getDato()
+        {
+            return this.dato;
+        }
+
+        public List<NodoGeneral<T>> getHijos()
+        {
+            return this.hijos;
+        }
+
+        public void setDato(T dato)
+        {
+            this.dato = dato;
+        }
+
+        public void setHijos(NodoGeneral<T> Hijo)
+        {
+            this.hijos.Add(Hijo);
+        }
+
+    }
+}
