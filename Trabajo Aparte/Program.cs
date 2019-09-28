2f5fbe7f7ff5c917bd7ef2a08606cc843fbad0ff
EmmaGrande <EmmaGrande@DESKTOP-C7GQBUO>
2019-09-28 01:50:20 -0300

    fdbdthd

--- Trabajo Aparte/Program.cs
+++ Trabajo Aparte/Program.cs
@@ -6,22 +6,63 @@ namespace Trabajo_Aparte
     {
         static void Main(string[] args)
         {
-            ArbolBinarioOrdenado abo = new ArbolBinarioOrdenado();
-            abo.Insertar(5);
-            abo.Insertar(4);
-            abo.Insertar(6);
-            abo.Insertar(2);
-            abo.Insertar(7);
-           
-            Console.WriteLine("Impresion preorden: ");
-            abo.ImprimirPre();
-            Console.WriteLine("Impresion entreorden: ");
-            abo.ImprimirEntre();
-            Console.WriteLine("Impresion postorden: ");
-            abo.ImprimirPost();
+
+            nodo primero = new nodo(0);
+            primero.incertaralfinal(primero,2);
+            
+            primero.incertaralfinal(primero, 3);
+            primero.incertaralfinal(primero, 4);
+            primero.incertaralfinal(primero, 5);
+            
+            primero.imprimirTodos(primero);
             Console.ReadKey();
-            Console.WriteLine("Hello World!");
-             
         }
     }
+    public class nodo
+    {
+        int info;
+        public nodo siguiente;
+        public nodo (int dato)
+        {
+            info = dato;
+        }
+        public void verNodo()
+        {
+            Console.WriteLine(info);
+        }
+        
+        public void IncertarEnPrimero(nodo siguiente,int dato)
+        {
+            
+            nodo temporal = new nodo(dato);
+            temporal.siguiente = siguiente;
+            siguiente = temporal;
+
+        }
+        public void imprimirTodos(nodo primero)
+        {
+            primero.verNodo();
+            while (primero.siguiente != null)
+            {
+                primero = primero.siguiente;
+                Console.Write(primero);
+            }
+        }
+        public void incertaralfinal(nodo primero,int dato)
+        {
+            nodo nuevo = new nodo(dato);
+            nodo anterior = primero;
+            nodo actual = primero;
+            if
+            while (actual.siguiente != null)
+            {
+                anterior = actual;
+                actual = actual.siguiente;
+            }
+            actual.siguiente = nuevo;
+        }
+    }
+
+        
+
 }
